009248f8636363f14d3f64d4480078a788408e93
Jeremy Browne <23028887+jeremy-browne@users.noreply.github.com>
2022-12-08 17:41:38 +1100

    Added collapsing menu

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("I never used this in the end, but hello if you're reading this!");
+const settings = document.getElementById("settings");
+const sublinks = document.getElementsByClassName("sub-link");
+const sidebar = document.getElementById("navbar");
+
+settings.addEventListener("click", (event) => {
+    Array.from(sublinks).forEach((elem) => {
+        console.log(elem.style.display);
+        console.log(elem);
+        if (elem.style.display === "flex") {
+            elem.style.display = "none";
+        } else {
+            elem.style.display = "flex";
+        }
+    })
+});
